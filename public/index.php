efda3e31e54a7bc915820c67aac10d8706f5bbdc
Dnvcomp <dnvcomp@hotmail.com>
2020-07-12 15:24:08 +0300

    Router - part One

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
+
 require_once dirname(__DIR__) . '/Config/init.php';
 require_once LIBS . '/functions.php';
+require_once  CONF . '/routes.php';
 new \dnvmaster\App();
-//debug(\dnvmaster\App::$app->getProperties());
-throw new Exception('Страница не найдена', 500);
