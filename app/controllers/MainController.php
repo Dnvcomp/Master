eb2b67ec5e34161b3d72e0176eaac9c347f00cc0
Dnvcomp <dnvcomp@hotmail.com>
2020-07-17 22:28:22 +0300

    10 --- Class View - Part two

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -8,6 +8,10 @@ class MainController extends AppController
 {
     public function indexAction()
     {
-        $this->setMeta(App::$app->getProperty('dnvmaster_name'),'Опсание странцы', 'Ключевые слова');
+        $this->setMeta(App::$app->getProperty('dnvmaster_name'),'Сервис DnvMaster, доступен каждому. В нём можно заказать услуги по ремонту, электрики, сборки мебели, сантехники и хозяйству в вашем доме или квартире. В течение двух часов мастер приедет к Вам и выполнит заказ качественно по приемлемой цене.', 'DnvMaster, Сервис, Заказ, Цена, Качество, Электрика, Сантехника, Мебель, Услуги');
+        $name = 'John';
+        $age = 30;
+        $names = ['Andrei','Mikolai'];
+        $this->set(compact('name','age', 'names'));
     }
 }
