84c8eec0d660899df1f912d85060d37b863196e1
Dnvcomp <dnvcomp@hotmail.com>
2020-07-19 20:09:28 +0300

    Finish create framework

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -3,6 +3,7 @@
 namespace app\controllers;
 
 use dnvmaster\App;
+use dnvmaster\Cache;
 
 class MainController extends AppController
 {
@@ -13,7 +14,16 @@ class MainController extends AppController
         $this->setMeta(App::$app->getProperty('dnvmaster_name'),'Сервис DnvMaster, доступен каждому. В нём можно заказать услуги по ремонту, электрики, сборки мебели, сантехники и хозяйству в вашем доме или квартире. В течение двух часов мастер приедет к Вам и выполнит заказ качественно по приемлемой цене.', 'DnvMaster, Сервис, Заказ, Цена, Качество, Электрика, Сантехника, Мебель, Услуги');
         $name = 'John';
         $age = 30;
-        $names = ['Andrei','Mikolai'];
+        $names = ['Alena','Mikolai','Irina'];
+        $cache = Cache::instance();
+        //$cache->set('tb_test', $names);
+        //$cache->delete('tb_test');
+        $data = $cache->get('tb_test');
+        if (!$data)
+        {
+            $cache->set('tb_test', $names);
+        }
+        debug($data);
         $this->set(compact('name','age', 'names', 'posts'));
     }
 }
